511c561971ad63a6c699f3d55242bec89f3dd20d
berkan bala <berkan.balaa@gmail.com>
2024-07-06 12:55:11 +0300

    directus-2

--- src/lib/directus.ts
+++ src/lib/directus.ts
@@ -1,8 +1,6 @@
-"use client";
-
 import { createDirectus, rest } from "@directus/sdk";
 
-const directus = createDirectus("http://localhost:8055/").with(
+const directus = createDirectus("http://172.20.112.1:8055/").with(
   rest({
     onRequest: (options) => ({ ...options, cache: "no-store" }),
   })
