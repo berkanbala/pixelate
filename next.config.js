7c43aa9059e8261af748b5a287c3b99953e084fd
berkan.balaa@gmail.com <berkan.balaa@gmail.com>
2024-07-15 15:59:59 +0300

    build

--- next.config.js
+++ next.config.js
@@ -1,19 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  experimental: {
-    images: {
-      unoptimized: true,
-      remotePatterns: [
-        {
-          protocol: "http",
-          hostname: "localhost",
-          port: "8055",
-          pathname: "/assets/**",
-        },
-      ],
-    },
+  // experimental: {
+  images: {
+    // unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "8055",
+        pathname: "/assets/**",
+      },
+    ],
   },
+  // },
 };
 
 module.exports = nextConfig;
